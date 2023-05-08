44ce79dd71c3bedd4dba03bceec86d9aac9dd90e
Piotr Libera <plibera@student.ethz.ch>
2023-05-08 16:10:35 +0000

    Update freiburg preprocessing

--- scripts/data_preprocessing/preprocess_freiburg_forest.py
+++ scripts/data_preprocessing/preprocess_freiburg_forest.py
@@ -1,29 +1,31 @@
 ############################################
-# RUGD preprocessing script
+# Freiburg Forest preprocessing script
 #
-# RUGD: http://rugd.vision/
-# Wigness, M., Eum, S., Rogers, J. G., Han, D., & Kwon, H. (2019).
-# A RUGD Dataset for Autonomous Navigation and Visual Perception in Unstructured Outdoor Environments. 
-# International Conference on Intelligent Robots and Systems (IROS). 
+# http://deepscene.cs.uni-freiburg.de/
+# Abhinav Valada, Gabriel L. Oliveira, Thomas Brox, Wolfram Burgard
+# Deep Multispectral Semantic Scene Understanding of Forested Environments using Multimodal Fusion
+# International Symposium on Experimental Robotics (ISER), Tokyo, Japan, 2016.
 #
 #
-#
-# Download RUGD:
-# wget http://rugd.vision/data/RUGD_frames-with-annotations.zip
-# wget http://rugd.vision/data/RUGD_annotations.zip
-#
-# unzip RUGD_frames-with-annotations.zip -d RUGD
-# unzip RUGD_annotations.zip -d RUGD
-# rm RUGD_annotations.zip RUGD_frames-with-annotations.zip
+# Download Friburg Forest:
+# wget http://deepscene.cs.uni-freiburg.de/static/datasets/download_freiburg_forest_annotated.sh
+# bash download_freiburg_forest_annotated.sh
+# tar -xzf freiburg_forest_annotated.tar.gz
+# rm freiburg_forest_annotated.tar.gz*
 # 
-# 
-# Expected input structure:
+#
+#
+# Expected input structure after unpacking in DATA_DIR:
 # DATA_DIR
 # |-- INPUT_NAME
-#     |-- RUGD_annotations
-#     |-- RUGD_frames-with-annotations
+#     |-- train
+#         |-- rgb
+#         |-- GT_color
+#     |-- test
+#         |-- rgb
+#         |-- GT_color
 #
-# Output structure:
+# Output structure after preprocessing:
 # DATA_DIR
 # |-- OUTPUT_NAME
 #     |-- imgs
@@ -44,8 +46,8 @@ from scripts.data_preprocessing.preprocessing_utils import *
 
 
 DATA_DIR="/media"
-INPUT_NAME="Freiburg_forest"
-OUTPUT_NAME="Freiburg_forest_preprocessed"
+INPUT_NAME="freiburg_forest_annotated"
+OUTPUT_NAME="freiburg_forest_preprocessed"
 
 
 FF_CMAP = np.array([(0, 0, 0),       # Object
@@ -55,32 +57,23 @@ FF_CMAP = np.array([(0, 0, 0),       # Object
                     (102, 102, 51)]) # Vegetation
 
 
-def preprocess_samples(input_dir, output_dir, subset, samples):
+def preprocess_samples(input_dir, output_dir, subset, input_subset):
     print("Processing subset {}".format(subset))
-    for sample in tqdm(samples):
-        img_path = join(input_dir, "RUGD_frames-with-annotations", sample)
-        label_path = join(input_dir, "RUGD_annotations", sample)
-        img_files = os.listdir(img_path)
-        label_files = os.listdir(label_path)
-        for img_file in img_files:
-            if img_file.endswith(('.png')):
-                preprocess_and_copy_image(join(img_path, img_file),
-                                          join(output_dir, "imgs", subset, img_file),
-                                          False)
-        print("Processing labels of sample {}".format(sample))
-        for label_file in tqdm(label_files):
-            if label_file.endswith(('.png')):
-                preprocess_and_copy_image(join(label_path, label_file),
-                                          join(output_dir, "labels", subset, label_file),
-                                          True, True, RUGD_CMAP)
+    label_names = os.listdir(join(input_dir, input_subset, "GT_color"))
+    for label_name in tqdm(label_names):
+        sample_name = label_name.split("_")[0]
+        img_path = join(input_dir, input_subset, "rgb", sample_name+"_Clipped.jpg")
+        label_path = join(input_dir, input_subset, "GT_color", label_name)
+        preprocess_and_copy_image(img_path, join(output_dir, "imgs", subset, sample_name+".jpg"), False)
+        preprocess_and_copy_image(label_path, join(output_dir, "labels", subset, sample_name+".png"), True, True, FF_CMAP)
 
 
 def main():
     input_dir = join(DATA_DIR, INPUT_NAME)
     output_dir = join(DATA_DIR, OUTPUT_NAME)
     create_dataset_structure(output_dir)
-    preprocess_samples(input_dir, output_dir, "train", TRAIN_SAMPLES+VAL_SAMPLES)
-    preprocess_samples(input_dir, output_dir, "val", TEST_SAMPLES)
+    preprocess_samples(input_dir, output_dir, "train", "train")
+    preprocess_samples(input_dir, output_dir, "val", "test")
 
 
 if __name__ == "__main__":
