dcfb169d0b1da241718e7669a4a527fbf1b7ec8f
Matias Mattamala <matias@robots.ox.ac.uk>
2024-04-12 21:38:38 +0100

    Ensure that weights are downloaded in right path

--- models/download_pretrained.sh
+++ models/download_pretrained.sh
@@ -8,4 +8,8 @@
 # This scripts gets the pretrained weights to run STEGO
 #
 
+# From https://stackoverflow.com/a/246128
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+cd $SCRIPT_DIR
+
 gdown https://drive.google.com/uc?id=1t6dS_9LlN9meN2yoA6iSZEaLlVMEJ4de
